6a303d423ae45c4bb81a8bad43d46ceac4dac109
Макс Михайленко <max.mykhailenko@gmail.com>
2013-04-27 06:48:10 +0300

    version 0.1.3

---
+++ README.md
@@ -0,0 +1,39 @@
+memTask
+=======
+
+memTask will remember all time which you spent in Sublime Text 2 and Sublime Text 3
+
+![interface](https://dl.dropbox.com/u/3478999/different/memTask.PNG "interface")
+
+Tested on:
+  - Windows 8
+  - Mac OS X 10.7.5
+  - Mac OS X 10.8.3
+
+WARNING! Reinstall plugin from package repository.
+If you have some problems on other OS please add issues.
+
+## Plans
+  - add comments to each file in tree (done 65%)
+  - add connector to redmine and other project management applications (done 25%)
+  - add grouping by month (week?)
+
+## Usage
+  - **mac and windows users**
+    - install plugin from package repository
+    - restart ST
+    - ctrl+alt+t (ctrl+super+t on mac) will show all time. You can save this file with specific date in some folder, add comments
+    - ctrl+shift+p "memTask: Show spent time"
+
+## Changelog
+  - **Version 0.1.3**
+    - Sublime text 3 support
+  - **Version 0.1.2**
+    - add date sorting
+    - fixed incorect time in projects views
+    - fixed using plugin on mac and windows in the same time
+  - **Version 0.1.1**
+    - folding by date or folder
+  - **Version 0.1**
+    - you can change idle time in settings
+    - current date as root of tree
---
+++ memTask.py
@@ -0,0 +1,236 @@
+# -*- coding: utf8 -*-
+import sublime
+import sublime_plugin
+import json
+import datetime
+from collections import defaultdict
+import platform
+import re
+from pprint import pprint
+from math import floor
+
+try:
+    # Python 3 have OrderedDict
+    from collections import OrderedDict
+except ImportError:
+    # Python 2 didn't have
+    from ordereddict import OrderedDict
+
+
+class memTask(sublime_plugin.EventListener):
+    def __init__(self):
+        if not hasattr(self, "setting") is None:
+            self.setting = {}
+
+        if platform.system() == 'Windows':
+            self.dirSep = "\\"
+        else:
+            self.dirSep = '/'
+
+        self.setting['file_path'] = self.dirSep + "User" + self.dirSep + "memTask.json"
+        self.stopTimer = True
+        self.fileName = False
+        self.fileView = False
+
+        if not sublime.version() or int(sublime.version()) > 3000:
+            # Sublime Text 3
+            timeout = 1000
+        else:
+            timeout = 0
+
+        sublime.set_timeout(lambda: self.finish_init(), timeout)
+
+    def finish_init(self):
+        settings = sublime.load_settings('memTask.sublime-settings')
+        self.setting['idle'] = settings.get('idle')
+        self.setting['date_format'] = settings.get('date_format')
+
+        self.today = datetime.datetime.now().strftime(self.setting['date_format'])
+        self.base = self.ReadBaseFromFile()
+
+    def ElapsedTime(self):
+        if self.stopTimer is False:
+            timeSec = (datetime.datetime.now() - self.lastChangeTime).seconds
+
+            if timeSec > self.setting['idle']:
+                self.stopTimer = True
+                return
+            else:
+                if self.fileName is None:
+                    self.fileName = 'temp files'
+                fp = self.today + self.dirSep + self.fileName
+                if fp in self.base:
+                    self.base[fp]["time"] = int(self.base[fp]["time"]) + int(5)
+                else:
+                    self.base[fp] = {
+                        "time": 5,
+                        "path_divider": self.dirSep
+                    }
+                self.SetStatus('elapsedTime', 'Elapsed time: ' + str(self.SecToHM(self.base[fp]["time"])))
+                sublime.set_timeout(lambda: self.ElapsedTime(), 5000)
+        else:
+            self.EraseStatus('elapsedTime')
+
+    def on_modified(self, view):
+        self.lastChangeTime = datetime.datetime.now()
+        if self.fileName is False or self.fileName is None:
+            self.fileName = view.file_name()
+        if self.fileView is False or self.fileView is None:
+            self.fileView = view
+        if self.stopTimer is True:
+            self.stopTimer = False
+            self.ElapsedTime()
+
+    def on_activated(self, view):
+        self.fileName = view.file_name()
+        self.fileView = view
+
+    def on_post_save(self, view):
+        self.WriteBaseToFile(self.base)
+
+    def SetStatus(self, place, phrase):
+        self.fileView.set_status(place, phrase)
+
+    def EraseStatus(self, place):
+        for view in sublime.active_window().views():
+            view.erase_status(place)
+
+    def SecToHM(self, seconds):
+        hours = floor(seconds / 3600)
+        seconds -= 3600 * hours
+        minutes = floor(seconds / 60)
+        return "%02d:%02d" % (hours, minutes)
+
+    def ReadBaseFromFile(self):
+        try:
+            with open(sublime.packages_path() + self.setting['file_path'], "r") as json_data:
+                data = json.load(json_data)
+                json_data.close()
+                return data
+        except IOError as e:
+            self.WriteBaseToFile({})
+            data = {}
+            print('memTask: Database file created.' + str(e))
+            return data
+
+    def WriteBaseToFile(self, data):
+        json_data_file = open(sublime.packages_path() + self.setting['file_path'], "w+")
+        json_data_file.write(json.dumps(data, indent=4, sort_keys=True))
+        json_data_file.close()
+
+MT = memTask()
+
+
+class ShowTimeCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        self.ShowReportVariants()
+
+    def treeify(self, seq, removeDate):
+        ret = {}
+        if removeDate:
+            newSeq = {}
+            for path in seq:
+                if "\\" in path:
+                    newPath = re.sub(r'^([^\\]+)\\', '', path)
+                else:
+                    newPath = re.sub(r'^([^/]+)/', '', path)
+                if newPath in newSeq:
+                    newSeq[newPath]["time"] = int(newSeq[newPath]["time"]) + seq[path]["time"]
+                else:
+                    newSeq[newPath] = seq[path]
+            seq = newSeq
+
+        for path in seq:
+            if not hasattr(seq[path], "path_divider"):
+                if "\\" in path:
+                    seq[path]["path_divider"] = "\\"
+                else:
+                    seq[path]["path_divider"] = "/"
+
+            seq[path]['pathArray'] = path.split(seq[path]["path_divider"])
+
+            # Не брать файлы с временных папок
+            if 'temp' not in seq[path]['pathArray'] and 'Temp' not in seq[path]['pathArray']:
+                cur = ret
+                for ind, node in enumerate(seq[path]['pathArray']):
+                    # Если последний элемент, то нужно взять время, а не детей
+                    if ind == len(seq[path]['pathArray']) - 1:
+                        cur = cur.setdefault(node, {'time': seq[path]['time']})
+                    else:
+                        cur = cur.setdefault(node, {})
+        return ret
+
+    def ShowGroupedBy(self, type):
+        view = self.window.new_file()
+        view.set_syntax_file('Packages/memTask/memTask.tmLanguage')
+        Tree = lambda: defaultdict(Tree)
+        tree = Tree()
+        self.base = MT.ReadBaseFromFile()
+
+        tree = self.treeify(self.base, False if type == 'date' else True)
+
+        view.run_command("update_mem_task_view", {'tree': tree, 'type': type})
+
+        view.set_name("all.time")
+
+    def ShowReportVariants(self):
+        self.variants = [
+            ['Group time by date'],
+            ['Group time by project']
+        ]
+
+        self.window.show_quick_panel(self.variants, self.VariantClick)
+
+    def VariantClick(self, picked):
+        if picked == -1:
+            return
+
+        if picked == 0:
+            self.ShowGroupedBy('date')
+            return
+
+        if picked == 1:
+            self.ShowGroupedBy('project')
+
+
+class UpdateMemTaskViewCommand(sublime_plugin.TextCommand):
+    def run(self, edit, tree, type):
+
+        # tree = OrderedDict(sorted(tree.items(), key=lambda k: datetime.datetime.strptime(k[0][:10], MT.setting['date_format'])))
+        if type == 'date':
+            tree = OrderedDict(sorted(tree.items(), key=lambda k: k[0][:10].split('.')[::-1], reverse=True))
+
+        self.view = self.view.window().active_view()
+        self.printLine(edit, tree, 0)
+        self.view.insert(edit, self.view.size(), "\n")
+
+    def IsDate(self, line):
+        try:
+            datetime.datetime.strptime(line, MT.setting['date_format'])
+            return True
+        except Exception:
+            return False
+
+    def printLine(self, edit, tree, level):
+        forkAmount = 0
+        for key, value in tree.items():
+            if key == 'time':
+                amount = value
+            else:
+                amount = 0
+                self.view.insert(edit, self.view.size(), "\n")
+                i = 0
+                while i < level:
+                    self.view.insert(edit, self.view.size(), u"  ")
+                    i += 1
+                self.view.insert(edit, self.view.size(), key)
+                tempViewSize = self.view.size()
+                if self.IsDate(key):
+                    MT.startFolding = self.view.size()
+                amount = self.printLine(edit, tree[key], level + 1)
+                self.view.insert(edit, tempViewSize, ': ' + MT.SecToHM(amount))
+                forkAmount += amount
+                if self.IsDate(key):
+                    if key != MT.today:
+                        self.view.fold(sublime.Region(MT.startFolding+7, self.view.size()))
+        return forkAmount or amount
